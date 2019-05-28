39b8ff776bdb9df7e7faabac78ebedcd603978f5
Griffin <griffincriste@gmail.com>
2019-05-28 18:03:12 -0500

    can't get anything from user input

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -2,54 +2,34 @@ $(document).ready(function() {
    var time = 10;
    var intervalId;
     var question ="";
- 
+ var correctAnswer=0;
+ var wrongAnswer=0;
 
 var maininfo;
+
+
     $("#startButton").on("click", function(){
-    reset();
-  loadQuestion();
+        $("#startButton").toggle();
+        $("#mainsection").toggle();
+        loadQuestion();
+        run();
 });
 
 
 
 
-    function reset(){
 
-    $("#mainsection").toggle();
-    $("#startButton").toggle();
-    question1();
-    console.log("hello")
- 
-};
-
-function clearQuestion(){
-   $(".answer1").text("");
-   $(".answer2").text("");
-   $(".answer3").text("");
-   $(".answer4").text("");
    
- 
-}
+
 
 function run(){
     clearInterval(intervalId);
-    time = 10;
+    time = 20;
     intervalId = setInterval (decrement,1000);
+    $(".time-remaining").text(time)
+
     
     }
-
-function stop(){
-    clearInterval(intervalId);
-    setTimeout(question2, 1000*5);
-
-}
-
-function timeOut(){
-    $("#out-of-time").text("You're Out of Time!")
-   stop();
-    
-    clearInterval(intervalId)
-}
 
 
 function decrement(){
@@ -60,99 +40,119 @@ function decrement(){
     }
 }
 
-
-
-
-
-
-function question1(){
-
-     var loadQuestion=maininfo[0].question;
-     $(".question").text(loadQuestion);
-    
-    $(".answer1").text(maininfo[0].answers[0]);
-    $(".answer2").text(maininfo[0].answers[1]);
-    $(".answer3").text(maininfo[0].answers[2]);
-    $(".answer4").text(maininfo[0].answers[3]);
-
-    $(".answer1").on("click", wrong1);
-    $(".answer2").on("click", wrong1);
-    $(".answer3").on("click", correct1);
-    $(".answer4").on("click", wrong1);
-    run();
-}
-
-
-
-function correct1(){
-    clearQuestion();
-    $(".question").text("Correct");
-    $(".correct-answer").text ("The Correct Answer was: "+ maininfo[0].correctGuess);
-    stop();
+function timeOut(){
+    $(".main").text("You are out of time!")
+    console.log("youre out of time")
+    clearInterval(intervalId)
+    gameOver();
 
 }
 
-function wrong1(){
-    clearQuestion();
-    $(".question").text("WRONG ANSWER!");
-    $(".correct-answer").text ("The Correct Answer was: "+ maininfo[0].correctGuess);
-    stop();
-    
-}
 
 
-function question2(){
-    
-    var loadQuestion=maininfo[1].question;
-    $(".question").text(loadQuestion);
-    $(".correct-answer").text("")
-
-   $(".answer1").text(maininfo[1].answers[0]);
-   $(".answer2").text(maininfo[1].answers[1]);
-   $(".answer3").text(maininfo[1].answers[2]);
-   $(".answer4").text(maininfo[1].answers[3]);
-
-   $(".answer1").on("click", wrong2);
-   $(".answer2").on("click", correct2);
-   $(".answer3").on("click", wrong2);
-   $(".answer4").on("click", wrong2);
-    run();
-}
-
-function correct2(){
-    clearQuestion();
-    $(".question").text("Correct!");
-    $(".correct-answer").text ("The Correct Answer was: "+ maininfo[1].correctGuess)
-    stop();
-}
 
 
-function wrong2(){
-    clearQuestion();
-    $(".question").text("WRONG ANSWER!")
-    $(".correct-answer").text ("The Correct Answer was: "+ maininfo[1].correctGuess)
-    stop();
-}
 
 
 function loadQuestion(){
 
     for (var i =0; i < maininfo.length ;i++){
         question  = maininfo[i].question;
-        answer=maininfo[i].answers;
-        correctGuess = maininfo[i].correctGuess;
-        gif=maininfo[i].gif;
-        $(".question").text(question)
-        $(".answer1").text(answer)
-
-        console.log(question)
-       console.log(answer)
+        
+       
+        $(".main").append(question +"<br>")
+        for (var j=0; j< maininfo[i].answers.length;j++){
+         $(".main").append("<input type ='radio'  name='question-"+i+"'  value='"+maininfo[i].answers[j]+"'>"  +maininfo[i].answers[j]+"<br>")
+        }
+        
+     
+      
     }
+
+    checkAnswer();
+}
+function checkAnswer(){
+   
+   
+
+    $("input[name='question-0']:checked"),function(){
+        if($(this).val()==maininfo[0].correctGuess){
+            correctAnswer++;
+            console.log(correctAnswer)
+        }
+        else {
+            wrongAnswer++;
+            console.log(wrongAnswer)
+        };
+      };
+
+    $.each($("input[name='question-1']:checked"),function(){
+        if($(this).val()==maininfo[1].correctGuess){
+            correctAnswer++;
+        }
+        else {
+            wrongAnswer++;
+        };
+      });
+
+    $.each($("input[name='question-2']:checked"),function(){
+      if($(this).val()==maininfo[2].correctGuess){
+          correctAnswer++;
+          console.log(correctAnswer)
+      }
+      else {
+          wrongAnswer++;
+          console.log(wrongAnswer)
+      };
+    });
+
+    $.each($("input[name='question-3']:checked"),function(){
+        if($(this).val()==maininfo[3].correctGuess){
+            correctAnswer++;
+        }
+        else {
+            wrongAnswer++;
+        };
+      });
+
+     $.each($("input[name='question-4']:checked"),function(){
+        if($(this).val()==maininfo[4].correctGuess){
+            correctAnswer++;
+        }
+        else {
+            wrongAnswer++;
+        };
+      });
+
+      $.each($("input[name='question-5']:checked"),function(){
+        if($(this).val()==maininfo[5].correctGuess){
+            correctAnswer++;
+        }
+        else {
+            wrongAnswer++;
+        }
+      });
+    
+
 }
 
 
 
 
+function gameOver(){
+    clearInterval(intervalId);
+    $(".main").remove();
+    $("#final-correct").text("Correct answers: " + correctAnswer);
+    $("#final-wrong").text("Incorrect answers: " + wrongAnswer);
+    
+    console.log(correctAnswer)
+    console.log(wrongAnswer)
+
+}
+
+$("#done").on("click", function(){
+    gameOver();
+})
 
 var maininfo = [{
     question: "Who wrote Stairway to Heaven?",
